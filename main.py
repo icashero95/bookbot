41e1b00537196ae22d2675473cf02ca78ca2db7a
icashero95 <iancasheroweb34@gmail.com>
2025-01-28 01:43:42 -0500

    Finished the creation of the bookbot

--- main.py
+++ main.py
@@ -2,10 +2,20 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
-    letter_count = get_char_count(text)
+    char_dict = get_char_count(text)
+    char_list = []
+    for char, count in char_dict.items():
+        new_dict = {"name": char, "num": count}
+        char_list.append(new_dict)
+    char_list.sort(reverse=True, key=sort_dict)
     print(f"=== Here's a report of {book_path} ===")
     print(f"{num_words} words were found in the document.")
-    
+    for i in char_list:
+        print(f"The '{i['name']}' character was found {i['num']} times")
+    print("=== End of report ===")
+
+def sort_dict(dict):
+    return dict["num"]
 
 def get_num_words(text):
     words = text.split()
@@ -19,9 +29,9 @@ def get_char_count(words):
     letters = words.lower()
     char_count = {}
     for letter in letters:
-        if letter in char_count:
+        if letter in char_count and letter.isalpha():
             char_count[letter] += 1
-        else:
+        elif letter not in char_count and letter.isalpha():
             char_count[letter] = 1
     return char_count
 
